72646cac7d73d7189741c623ba66abae8bb3e05c
Abhay kumawat <abhaykumawat81@gmail.com>
2024-03-04 16:27:28 +0530

    First Commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center mt-5">
+        <span className="text-blue-600 ">Why</span> Choose Us
+      </h1>
+      <div className="flex justify-center">
+        <div className="grid mt-5 md:grid-cols-2 grid-cols-1 place-items-center-start">
+          <div className="box w-[350px] sm:w-[375px] h-[275px]  m-5 relative overflow-hidden bg-[url(https://s3-alpha-sig.figma.com/img/368d/1399/92093f343d52ef035de6e1beed4d0e39?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cdAvCDixdD28vs1ANEHjGSzvnCwYNCq2EFJIgw59wdIF24KOUSSYIwLSJlTS36sDKpdrWr4zx3iBsGYv-hBX2ICP0g5EPUgfDYtVfIeCjUA963StLvC~2~1gkelZz~6Ws19z3PbEunDgf4jqm64VIh1f9LiyQ~S~vOf69g4fHEQeYe5g~2P2bVDvZsc8JhLfY8JThCGt6IzJAgNw6L9890FT6wFCJChfmfgZWgGeVzDlHt~dh4kdvMsQIUx6Y9Qf39viOuW3tJbI5Mi7-wxpcFbe9NhA4tfnvxaKZEfhAb3dyIE7XhddBJkn60pZJOHLzQIHFSrX-UrjW91vqjv2Zg__)] bg-no-repeat bg-cover filter brightness-20">
+            <h1 className="text-xl font-semibold text-white absolute bottom-2 text-center left-16">
+              Striking the{" "}
+              <span className="text-yellow-500 ">Perfect Balance</span>
+            </h1>
+            <div className="overlay absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 text-white text-center opacity-0 transition-opacity duration-300 flex justify-center items-center hover:bg-blue-500 hover:opacity-100 hover:bg-opacity-90 delay-75 ease-in-out">
+              <p className="text-center">
+                {" "}
+                <span className="text-3xl">
+                  <span className="text-yellow-500">Empower</span>, Explore,{" "}
+                  <span className="text-yellow-500">Excel</span>
+                </span>
+                <br />
+                Experience innovation in education! Our <br />
+                interactive approach, featuring audio/visual
+                <br /> lectures, group discussions, and projects,
+                <br /> nurtures critical thinking and creativity
+              </p>
+            </div>
+          </div>
+          <div className="box w-[350px] sm:w-[375px] h-[275px] m-5 relative overflow-hidden bg-[url(https://s3-alpha-sig.figma.com/img/31ef/75cb/3cd5a00b2a5499627e4623e04dd84704?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eSlZ8g-5jZJFNkwSahxP8ktE2~tInF8vp~m0KR5I4OTgcts6W2UZV0MWIkn8agVExUJN1FbvpLWf65iVs0cEyQv13aw9Z7YwioS7h7ERFR8PkFu3fJADHFxeaVSSVyV1naNUWfOdb~nwl6ljkoo~D1L3VrUjx8XGtY45LppkwBcs5UVqmS0G6qu~mGsSKPOaocPHBTYVwTMcUR61XVhBt71kZDKvHectngqkek3RNCtnlInyepHcm0KyP5rnJVV5PbLHJcitKJl7uQbxSAspi1yMK2qY-r-6As18tsdML4WJ6aFFFGyv0jBC4fagaB-W5icH2jQvN6yXdo~G-vINNw__)] bg-no-repeat bg-cover filter brightness-20">
+            <h1 className="text-xl font-semibold text-white absolute bottom-2 text-center left-16">
+              Striking the{" "}
+              <span className="text-yellow-500 ">Perfect Balance</span>
+            </h1>
+            <div className="overlay absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 text-white text-center opacity-0 transition-opacity duration-300 flex justify-center items-center hover:bg-blue-500 hover:opacity-100 hover:bg-opacity-90 delay-75 ease-in-out">
+              <p className="text-center">
+                {" "}
+                <span className="text-3xl">
+                  <span className="text-yellow-500">Empower</span>, Explore,{" "}
+                  <span className="text-yellow-500">Excel</span>
+                </span>
+                <br />
+                Experience innovation in education! Our <br />
+                interactive approach, featuring audio/visual
+                <br /> lectures, group discussions, and projects,
+                <br /> nurtures critical thinking and creativity
+              </p>
+            </div>
+          </div>
+          <div className="box w-[350px] sm:w-[375px] h-[275px] m-5 relative overflow-hidden bg-[url(https://s3-alpha-sig.figma.com/img/fb98/69fe/0c4b7871df848d70bfc042d65a7cf9b6?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=O2GA5ZVV9N6iTKz~bixvalM45XUiEbL69VRWuqELOaa3g~~Nb6nLlkdlqaNV1xcUQqhK7qFrVIUnou9bG1vILWprBrfooexWrBCd6LDfXr-3yw1D~9GViGACuVXFv7RMSuQ5Tmt4iBg54bAljVPKRg~H9~6TrGwXEYNdh8-dUz0vwkVoY~LXCKFU~QCdkzreoz1VDSZmYzzAK4PEZUbq98fZ5NdC8BVe7HGHVn5P9i19je6K-xJAOgfn9GcJw0gs17DX9XPeHpEXAVRdEAU5wzJZPg-1o8JtFL1NIsjBDnd3GNZL~B5pyZz8SiiktcMZiKFPRzh54-bmRcYWR9lLyQ__)] bg-no-repeat bg-cover filter brightness-20">
+            <h1 className="text-xl font-semibold text-white absolute bottom-2 text-center left-16">
+              Striking the{" "}
+              <span className="text-yellow-500 ">Perfect Balance</span>
+            </h1>
+            <div className="overlay absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 text-white text-center opacity-0 transition-opacity duration-300 flex justify-center items-center hover:bg-blue-500 hover:opacity-100 hover:bg-opacity-90 delay-75 ease-in-out">
+              <p className="text-center">
+                {" "}
+                <span className="text-3xl">
+                  <span className="text-yellow-500">Empower</span>, Explore,{" "}
+                  <span className="text-yellow-500">Excel</span>
+                </span>
+                <br />
+                Experience innovation in education! Our <br />
+                interactive approach, featuring audio/visual
+                <br /> lectures, group discussions, and projects,
+                <br /> nurtures critical thinking and creativity
+              </p>
+            </div>
+          </div>
+          <div className="box w-[350px] sm:w-[375px] h-[275px] m-5 relative overflow-hidden bg-[url(https://s3-alpha-sig.figma.com/img/80b9/12a4/f7cae889d523de7ff084c5905a18c718?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fEpTEUqx3ol7vCH5Zs~cTW038wr~PlrT~T5ztUe9hCn8xuXpJWg60CNNkzGgRNXzve4FepDjO35B-03mX1g-6BgjRbbsPu74uUgRuBZnuwAK0Rj1oGa2Xi469Ucd4iqAJbzIX1zx-Ugb09vVnNxXeUpFt2le8w2V9rggtmQ6-sbrVJLMz67Brg04lvKd4u-jEH9RqVezHCS41zIpcWIwwXQHxoFHzVlbGVr~lHTO0vhZxjpSIb2ZSTvPB3tjM8J6lVOplICtdRuoC~DAOceymaBznv~fRZ6GrCOBHH4MsL1R6uBjL5JrppkGASDCmKldulsG0CUKst7vSfx4x0TOLQ__)] bg-no-repeat bg-cover filter brightness-20">
+            <h1 className="text-xl font-semibold text-white absolute bottom-2 text-center left-16">
+              Striking the{" "}
+              <span className="text-yellow-500 ">Perfect Balance</span>
+            </h1>
+            <div className="overlay absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 text-white text-center opacity-0 transition-opacity duration-300 flex justify-center items-center hover:bg-blue-500 hover:opacity-100 hover:bg-opacity-90 delay-75 ease-in-out">
+              <p className="text-center">
+                {" "}
+                <span className="text-3xl">
+                  <span className="text-yellow-500">Empower</span>, Explore,{" "}
+                  <span className="text-yellow-500">Excel</span>
+                </span>
+                <br />
+                Experience innovation in education! Our <br />
+                interactive approach, featuring audio/visual
+                <br /> lectures, group discussions, and projects,
+                <br /> nurtures critical thinking and creativity
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
